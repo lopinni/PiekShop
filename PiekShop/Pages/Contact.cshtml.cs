0cc50daccf262defe4aa8f9ca4ab0676b5ff7819
lopinni <lopinni@users.noreply.github.com>
2021-12-04 21:26:23 +0100

    Dodano stronę kontaktu

---
+++ PiekShop/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PiekShop.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
